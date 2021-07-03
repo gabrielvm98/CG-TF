366fba7f07b4d0537d938d997b870d21a479462a
gabrielvm98 <64874245+gabrielvm98@users.noreply.github.com>
2021-07-02 22:18:40 -0500

    Add files via upload

---
+++ include/figures.h
@@ -0,0 +1,98 @@
+#ifndef __FIGURES_H__
+#define __FIGURES_H__
+
+struct Rgb {
+	f32 r, g, b;
+};
+
+class Object {
+
+protected:
+	f32* vertices;
+	u32* indices;
+	u32  nv;
+	u32  ni;
+	u32  n;
+
+public:
+	Object(u32 n, u32 m)
+		: vertices(new f32[24 * n]), indices(new u32[m]), nv(24 * n), ni(m), n(n) {}
+
+	f32* getVertices() {
+		return vertices;
+	}
+	u32* getIndices() {
+		return indices;
+	}
+	u32 getVSize() {
+		return nv;
+	}
+	u32 getISize() {
+		return ni;
+	}
+	u32 len() {
+		return sizeof(f32)*n;
+	}
+	void* skip(u32 i) {
+		return (void*)(i * sizeof(u32));
+	}
+};
+
+class Cube : public Object {
+
+public:
+	Cube(f32 width = 1.0f, f32 height = 1.0f, f32 depth = 1.0f,
+		Rgb a = { 0.8f, 0.9f, 1.0f }, Rgb b = { 0.9f, 0.1f, 0.2f },
+		Rgb c = { 0.8f, 0.8f, 0.2f }, Rgb d = { 0.3f, 0.9f, 0.8f },
+		Rgb e = { 1.0f, 0.5f, 0.2f }, Rgb f = { 0.2f, 0.9f, 0.3f })
+		: Object(11, 36) {
+		f32 wm = width / 2.0f;
+		f32 hm = height / 2.0f;
+		f32 dm = depth / 2.0f;
+		f32 temp[] = {
+			// posiciones       colores           normals          texturas
+			-wm,  hm,  dm,   a.r, a.g, a.b,   0.0f, 0.0f, 1.0f,   0.0f, 1.0f,  // 0
+			 wm,  hm,  dm,   a.r, a.g, a.b,   0.0f, 0.0f, 1.0f,   1.0f, 1.0f,  // 1
+			-wm, -hm,  dm,   a.r, a.g, a.b,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,  // 2
+			 wm, -hm,  dm,   a.r, a.g, a.b,   0.0f, 0.0f, 1.0f,   1.0f, 0.0f,  // 3
+
+			-wm,  hm,  dm,   b.r, b.g, b.b,    0.0f, 1.0f, 0.0f,  0.0f, 1.0f,  // 4
+			 wm,  hm,  dm,   b.r, b.g, b.b,    0.0f, 1.0f, 0.0f,  1.0f, 1.0f,  // 5
+			-wm,  hm, -dm,   b.r, b.g, b.b,    0.0f, 1.0f, 0.0f,  0.0f, 0.0f,  // 6
+			 wm,  hm, -dm,   b.r, b.g, b.b,    0.0f, 1.0f, 0.0f,  1.0f, 0.0f,  // 7
+
+			 wm,  hm,  dm,   c.r, c.g, c.b,    1.0f, 0.0f, 0.0f,  1.0f, 1.0f,  // 8
+			 wm,  hm, -dm,   c.r, c.g, c.b,    1.0f, 0.0f, 0.0f,  1.0f, 0.0f,  // 9
+			 wm, -hm,  dm,   c.r, c.g, c.b,    1.0f, 0.0f, 0.0f,  0.0f, 1.0f,  // 10
+			 wm, -hm, -dm,   c.r, c.g, c.b,    1.0f, 0.0f, 0.0f,  0.0f, 0.0f,  // 11
+
+			-wm, -hm,  dm,   d.r, d.g, d.b,    0.0f, -1.0f, 0.0f,  0.0f, 1.0f,  // 12
+			 wm, -hm,  dm,   d.r, d.g, d.b,    0.0f, -1.0f, 0.0f,  1.0f, 1.0f,  // 13
+			-wm, -hm, -dm,   d.r, d.g, d.b,    0.0f, -1.0f, 0.0f,  0.0f, 0.0f,  // 14
+			 wm, -hm, -dm,   d.r, d.g, d.b,    0.0f, -1.0f, 0.0f,  1.0f, 0.0f,  // 15
+
+			-wm,  hm,  dm,   e.r, e.g, e.b,    -1.0f, 0.0f, 0.0f,  1.0f, 1.0f,  // 16
+			-wm,  hm, -dm,   e.r, e.g, e.b,    -1.0f, 0.0f, 0.0f,  1.0f, 0.0f,  // 17
+			-wm, -hm,  dm,   e.r, e.g, e.b,    -1.0f, 0.0f, 0.0f,  0.0f, 1.0f,  // 18
+			-wm, -hm, -dm,   e.r, e.g, e.b,    -1.0f, 0.0f, 0.0f,  0.0f, 0.0f,  // 19
+
+			-wm,  hm, -dm,   f.r, f.g, f.b,    0.0f, 0.0f, -1.0f,  0.0f, 1.0f,  // 20
+			 wm,  hm, -dm,   f.r, f.g, f.b,    0.0f, 0.0f, -1.0f,  1.0f, 1.0f,  // 21
+			-wm, -hm, -dm,   f.r, f.g, f.b,    0.0f, 0.0f, -1.0f,  0.0f, 0.0f,  // 22
+			 wm, -hm, -dm,   f.r, f.g, f.b,    0.0f, 0.0f, -1.0f,  1.0f, 0.0f   // 23
+		};
+		for (u32 i = 0; i < nv; ++i) {
+			vertices[i] = temp[i];
+		}
+		for (u32 i = 0; i < 6; ++i) {
+			indices[i * 6 + 0] = i * 4 + 0;
+			indices[i * 6 + 1] = i * 4 + 1;
+			indices[i * 6 + 2] = i * 4 + 2;
+			indices[i * 6 + 3] = i * 4 + 1;
+			indices[i * 6 + 4] = i * 4 + 2;
+			indices[i * 6 + 5] = i * 4 + 3;
+		}
+	}
+};
+
+#endif
---
+++ include/glutil.h
@@ -0,0 +1,210 @@
+#ifndef __GLUTIL_H__
+#define __GLUTIL_H__
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <stb_image.h>
+#include <path.h>
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+typedef              float f32;
+typedef             double f64;
+typedef               char i8;
+typedef          short int i16;
+typedef                int i32;
+typedef          long long i64;
+typedef unsigned      char u8;
+typedef unsigned short int u16;
+typedef unsigned       int u32;
+typedef unsigned long long u64;
+
+void framebuffer_size_callback(GLFWwindow* window, i32 w, i32 h) {
+	glViewport(0, 0, w, h);
+}
+
+GLFWwindow* glutilInit(i32 major, i32 minor, i32 w, i32 h, const i8* title,
+	i32 screenWidth = 1366, i32 screenHeight = 768) {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, major);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, minor);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(w, h, title, nullptr, nullptr);
+	glfwSetWindowPos(window, screenWidth / 2 - w / 2, screenHeight / 2 - h / 2);
+	if (window == nullptr) {
+		std::cerr << "Failed to create GLFW Window\n";
+		glfwTerminate();
+		return nullptr;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cerr << "Not GLAD at all!\n";
+		return nullptr;
+	}
+
+	return window;
+}
+
+class Shader {
+public:
+	u32   pid;
+	Path* path;
+
+	std::vector<u32> textures;
+
+	// check for error status
+	i32 ok;
+	i8  infoLog[512];
+
+public:
+	Shader(std::string vertexFileName = "shader.vert",
+		std::string fragmentFileName = "shader.frag",
+		std::string shadersPath = "resources/shaders",
+		std::string texturesPath = "resources/textures")
+		: path(new Path(shadersPath, texturesPath)) {
+		std::ifstream vertexFile(path->sp(vertexFileName));
+		std::string vertexSrc;
+		std::getline(vertexFile, vertexSrc, '\0');
+
+		std::ifstream fragmentFile(path->sp(fragmentFileName));
+		std::string fragmentSrc;
+		std::getline(fragmentFile, fragmentSrc, '\0');
+
+		u32 vertex = mkShader(vertexSrc.c_str(), GL_VERTEX_SHADER);
+		u32 fragment = mkShader(fragmentSrc.c_str(), GL_FRAGMENT_SHADER);
+
+		pid = glCreateProgram();
+		glAttachShader(pid, vertex);
+		glAttachShader(pid, fragment);
+		glLinkProgram(pid);
+		glGetProgramiv(pid, GL_LINK_STATUS, &ok);
+		if (!ok) {
+			glGetProgramInfoLog(pid, 512, nullptr, infoLog);
+			std::cout << "Error::shader::program::link_failed\n"
+				<< infoLog << std::endl;
+		}
+
+		glDeleteShader(vertex);
+		glDeleteShader(fragment);
+	}
+	~Shader() {
+		glDeleteProgram(pid);
+		glfwTerminate();
+	}
+	void useProgram() {
+		glUseProgram(pid);
+	}
+	u32 getProgram() { // might need to refactor this later ughhh
+		return pid;
+	}
+
+	// Set uniforms
+	void setI32(const i8* name, const i32& i) const {
+		glUniform1i(glGetUniformLocation(pid, name), i);
+	}
+	void setF32(const i8* name, const f32& f) const {
+		glUniform1f(glGetUniformLocation(pid, name), f);
+	}
+	void setVec3(const i8* name, glm::vec3& vec) const {
+		glUniform3fv(glGetUniformLocation(pid, name), 1, &vec[0]);
+	}
+	void setVec3(const i8* name, f32 a, f32 b, f32 c) const {
+		glUniform3f(glGetUniformLocation(pid, name), a, b, c);
+	}
+	void setMat4(const i8* name, const glm::mat4& mat) const {
+		glUniformMatrix4fv(glGetUniformLocation(pid, name), 1, GL_FALSE, &mat[0][0]);
+	}
+
+	// Texture loading
+	u32 loadTexture(const std::string& textureFile,
+		const std::string& uniformName = "",
+		i32 param = GL_LINEAR) {
+		u32 texture;
+		std::string fileName = path->tp(textureFile);
+
+		glGenTextures(1, &texture);
+
+		i32 w, h, nrChannels;
+
+		stbi_set_flip_vertically_on_load(true); // porque en opgl el eje Y invertio
+		u8* data = stbi_load(fileName.c_str(), &w, &h, &nrChannels, 0);
+		if (data == nullptr) {
+			std::cerr << "Can't load texture\n";
+			return -1;
+		}
+		GLenum fmt;
+
+		if (nrChannels == 4) {
+			fmt = GL_RGBA;
+		}
+		else if (nrChannels == 3) {
+			fmt = GL_RGB;
+		}
+		else {
+			fmt = GL_RED;
+		}
+
+		glBindTexture(GL_TEXTURE_2D, texture);
+		glTexImage2D(GL_TEXTURE_2D, 0, fmt, w, h, 0, fmt, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+			GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, param);
+		stbi_image_free(data);
+		useProgram();
+		if (uniformName != "") {
+			setI32(uniformName.c_str(), textures.size());
+		}
+		textures.push_back(texture);
+
+		return texture;
+	}
+	void activeTexture(u32 pos) {
+		glActiveTexture(gl_texture(pos));
+		glBindTexture(GL_TEXTURE_2D, textures[pos]);
+	}
+
+private:
+	u32 mkShader(const i8* source, GLenum type) {
+		u32 shader = glCreateShader(type);
+		glShaderSource(shader, 1, &source, nullptr);
+		glCompileShader(shader);
+		glGetShaderiv(shader, GL_COMPILE_STATUS, &ok);
+		if (!ok) {
+			glGetShaderInfoLog(shader, 512, nullptr, infoLog);
+			std::cerr << "Error::shader::compilation_failed\n"
+				<< infoLog << std::endl;
+			return 0;
+		}
+		return shader;
+	}
+	i32 gl_texture(u32 pos) {
+		switch (pos) {
+		case 0: return GL_TEXTURE0;
+		case 1: return GL_TEXTURE1;
+		case 2: return GL_TEXTURE2;
+		case 3: return GL_TEXTURE3;
+		case 4: return GL_TEXTURE4;
+		case 5: return GL_TEXTURE5;
+		case 6: return GL_TEXTURE6;
+		case 7: return GL_TEXTURE7;
+		case 8: return GL_TEXTURE8;
+		case 9: return GL_TEXTURE9;
+		}
+		return -1;
+	}
+};
+
+#endif
